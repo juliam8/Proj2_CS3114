36621aacc6919a4664582165fc7f716f5458be84
Julia Mahon Kuzin <juliam8@vt.edu>
2019-03-18 19:01:13 -0400

    added testing for internal node

--- CS3114-Project2/src/InternalNodeTest.java
+++ CS3114-Project2/src/InternalNodeTest.java
@@ -29,26 +29,84 @@ public class InternalNodeTest {
 
     /**
      * Test method for {@link InternalNode#InternalNode(DNATreeNode, DNATreeNode, DNATreeNode, DNATreeNode, DNATreeNode)}.
-     */
+     
     @Test
     public void testInternalNodeDNATreeNodeDNATreeNodeDNATreeNodeDNATreeNodeDNATreeNode() {
         fail("Not yet implemented");
-    }
+    }*/
 
     /**
      * Test method for {@link InternalNode#insert(char[], int)}.
      */
     @Test
     public void testInsert() {
-        fail("Not yet implemented");
+        InternalNode n = new InternalNode();
+        char[] seq1 = new char[]{'A', 'A', 'C', 'T'};
+        n.insert(seq1, 1);
+        char[] seq2 = new char[]{'C', 'A', 'C', 'T'};
+        n.insert(seq2, 1);
+        char[] seq3 = new char[]{'G', 'G', 'C'};
+        n.insert(seq3, 1);
+        
+        assertFalse(n.a().isFlyweight());
+        assertFalse(n.c().isFlyweight());
+        assertFalse(n.g().isFlyweight());
+        assertTrue(n.a().isLeaf());
+        assertTrue(n.c().isLeaf());
+        assertTrue(n.g().isLeaf());
+        
+        assertEquals(((LeafNode) ((InternalNode) n).a()).dna(), seq1);
+        assertEquals(((LeafNode) ((InternalNode) n).c()).dna(), seq2);
+        assertEquals(((LeafNode) ((InternalNode) n).g()).dna(), seq3);
+        
+        char[] seq4 = new char[]{'C', 'G', 'C'};
+        n.insert(seq4, 1);
+        char[] seq5 = new char[]{'C', 'T', 'C'};
+        n.insert(seq5, 1);
+        
+        assertFalse(n.c().isLeaf());
+        assertTrue(((LeafNode) ((InternalNode) n.c()).a()).isLeaf());
+        assertTrue(((LeafNode) ((InternalNode) n.c()).g()).isLeaf());
+        assertTrue(((LeafNode) ((InternalNode) n.c()).t()).isLeaf());
+        assertEquals(((LeafNode) ((InternalNode) n.c()).a()).dna(), seq2);
+        assertEquals(((LeafNode) ((InternalNode) n.c()).g()).dna(), seq4);
+        assertEquals(((LeafNode) ((InternalNode) n.c()).t()).dna(), seq5);   
     }
-
+    
     /**
      * Test method for {@link InternalNode#remove(char[], int)}.
      */
     @Test
     public void testRemove() {
-        fail("Not yet implemented");
+        InternalNode n = new InternalNode();
+        char[] seq1 = new char[]{'A', 'A', 'C', 'T'};
+        n.insert(seq1, 1);
+        char[] seq2 = new char[]{'C', 'A', 'C', 'T'};
+        n.insert(seq2, 1);
+        char[] seq3 = new char[]{'G', 'G', 'C'};
+        n.insert(seq3, 1);
+        
+        assertTrue(n.a().isLeaf());
+        assertTrue(n.c().isLeaf());
+        assertTrue(n.g().isLeaf());
+        
+        n.remove(seq1, 1);
+        
+        assertFalse(n.a().isLeaf());
+        assertTrue(n.c().isLeaf());
+        assertTrue(n.g().isLeaf());
+        
+        n.remove(seq2, 1);
+        
+        assertFalse(n.a().isLeaf());
+        assertFalse(n.c().isLeaf());
+        assertTrue(n.g().isLeaf());
+        
+        n.remove(seq3, 1);
+        
+        assertFalse(n.a().isLeaf());
+        assertFalse(n.c().isLeaf());
+        assertFalse(n.g().isLeaf());
     }
 
     /**
@@ -56,7 +114,26 @@ public class InternalNodeTest {
      */
     @Test
     public void testSearch() {
-        fail("Not yet implemented");
+        InternalNode n = new InternalNode();
+        char[] seq1 = new char[]{'A', 'A', 'C', 'T'};
+        n.insert(seq1, 1);
+        char[] seq2 = new char[]{'C', 'A', 'C', 'T'};
+        n.insert(seq2, 1);
+        char[] seq3 = new char[]{'G', 'G', 'C'};
+        n.insert(seq3, 1);
+        char[] seq4 = new char[]{'C', 'G', 'C'};
+        n.insert(seq4, 1);
+        char[] seq5 = new char[]{'C', 'T', 'C'};
+        n.insert(seq5, 1);
+        
+        SequenceSearch curSearch = new SequenceSearch();
+        curSearch.sequenceFound = false;
+        curSearch.exactMatch = true;
+        
+        n.search(seq1, 1, curSearch);
+        assertTrue(curSearch.sequenceFound);
+        assertTrue(curSearch.exactMatch);
+        assertEquals(curSearch.getNumberOfNodesVisited(), 2);
     }
 
     /**
@@ -64,7 +141,26 @@ public class InternalNodeTest {
      */
     @Test
     public void testPrint() {
-        fail("Not yet implemented");
+        InternalNode n = new InternalNode();
+        char[] seq1 = new char[]{'A', 'A', 'C', 'T'};
+        n.insert(seq1, 1);
+        char[] seq2 = new char[]{'C', 'A', 'C', 'T'};
+        n.insert(seq2, 1);
+        char[] seq3 = new char[]{'G', 'G', 'C'};
+        n.insert(seq3, 1);
+        char[] seq4 = new char[]{'C', 'G', 'C'};
+        n.insert(seq4, 1);
+        char[] seq5 = new char[]{'C', 'T', 'C'};
+        n.print(false, false);
+        
+        assertFalse(n.c().isLeaf());
+        assertTrue(((LeafNode) ((InternalNode) n.c()).a()).isLeaf());
+        assertTrue(((LeafNode) ((InternalNode) n.c()).g()).isLeaf());
+        assertEquals(((LeafNode) ((InternalNode) n.c()).a()).dna(), seq2);
+        assertEquals(((LeafNode) ((InternalNode) n.c()).g()).dna(), seq4);
+        assertEquals(((LeafNode) ((InternalNode) n.c()).t()).dna(), seq5);   
+        
+
     }
 
     /**
@@ -72,7 +168,16 @@ public class InternalNodeTest {
      */
     @Test
     public void testSetA() {
-        fail("Not yet implemented");
+        InternalNode n = new InternalNode();
+        char[] seq1 = new char[]{'A', 'A', 'C', 'T'};
+        DNATreeNode a = new LeafNode(seq1);
+        n.setA(a);
+        
+        assertTrue(n.c().isFlyweight());
+        assertTrue(n.g().isFlyweight());
+        assertTrue(n.t().isFlyweight());
+        assertTrue(n.$().isFlyweight());
+        assertEquals(((LeafNode) ((InternalNode) n).a()).dna(), seq1);
     }
 
     /**
@@ -80,7 +185,16 @@ public class InternalNodeTest {
      */
     @Test
     public void testSetG() {
-        fail("Not yet implemented");
+        InternalNode n = new InternalNode();
+        char[] seq1 = new char[]{'G', 'A', 'C', 'T'};
+        DNATreeNode g = new LeafNode(seq1);
+        n.setG(g);
+        
+        assertTrue(n.c().isFlyweight());
+        assertTrue(n.a().isFlyweight());
+        assertTrue(n.t().isFlyweight());
+        assertTrue(n.$().isFlyweight());
+        assertEquals(((LeafNode) ((InternalNode) n).g()).dna(), seq1);
     }
 
     /**
@@ -88,7 +202,16 @@ public class InternalNodeTest {
      */
     @Test
     public void testSetC() {
-        fail("Not yet implemented");
+        InternalNode n = new InternalNode();
+        char[] seq1 = new char[]{'C', 'A', 'C', 'T'};
+        DNATreeNode c = new LeafNode(seq1);
+        n.setC(c);
+        
+        assertTrue(n.a().isFlyweight());
+        assertTrue(n.g().isFlyweight());
+        assertTrue(n.t().isFlyweight());
+        assertTrue(n.$().isFlyweight());
+        assertEquals(((LeafNode) ((InternalNode) n).c()).dna(), seq1);
     }
 
     /**
@@ -96,7 +219,16 @@ public class InternalNodeTest {
      */
     @Test
     public void testSetT() {
-        fail("Not yet implemented");
+        InternalNode n = new InternalNode();
+        char[] seq1 = new char[]{'T', 'T', 'C', 'T'};
+        DNATreeNode t = new LeafNode(seq1);
+        n.setT(t);
+        
+        assertTrue(n.c().isFlyweight());
+        assertTrue(n.g().isFlyweight());
+        assertTrue(n.a().isFlyweight());
+        assertTrue(n.$().isFlyweight());
+        assertEquals(((LeafNode) ((InternalNode) n).t()).dna(), seq1);
     }
 
     /**
@@ -104,7 +236,20 @@ public class InternalNodeTest {
      */
     @Test
     public void testSet$() {
-        fail("Not yet implemented");
+        InternalNode n = new InternalNode();
+        char[] seq1 = new char[]{'T', 'C'};
+        n.insert(seq1, 1);
+        char[] seq2 = new char[]{'T', 'G'};
+        n.insert(seq2, 1);
+        
+        char[] seq3 = new char[]{'T'};
+        DNATreeNode exact = new LeafNode(seq3);
+        ((InternalNode) n.t()).set$(exact);
+        
+        assertTrue(n.c().isFlyweight());
+        assertTrue(n.g().isFlyweight());
+        assertTrue(n.a().isFlyweight());
+        assertEquals(((LeafNode) ((InternalNode) n.t()).$()).dna(), seq3);
     }
 
     /**
@@ -112,7 +257,17 @@ public class InternalNodeTest {
      */
     @Test
     public void testA() {
-        fail("Not yet implemented");
+        InternalNode n = new InternalNode();
+        char[] seq1 = new char[]{'A', 'A', 'C', 'T'};
+        n.insert(seq1, 1);
+        char[] seq2 = new char[]{'C', 'A', 'C', 'T'};
+        n.insert(seq2, 1);
+        char[] seq3 = new char[]{'G', 'G', 'C'};
+        n.insert(seq3, 1);
+        char[] seq4 = new char[]{'T', 'G', 'C'};
+        n.insert(seq4, 1);
+        
+        assertEquals(((LeafNode) ((InternalNode) n).a()).dna(), seq1);
     }
 
     /**
@@ -120,7 +275,17 @@ public class InternalNodeTest {
      */
     @Test
     public void testG() {
-        fail("Not yet implemented");
+        InternalNode n = new InternalNode();
+        char[] seq1 = new char[]{'A', 'A', 'C', 'T'};
+        n.insert(seq1, 1);
+        char[] seq2 = new char[]{'C', 'A', 'C', 'T'};
+        n.insert(seq2, 1);
+        char[] seq3 = new char[]{'G', 'G', 'C'};
+        n.insert(seq3, 1);
+        char[] seq4 = new char[]{'T', 'G', 'C'};
+        n.insert(seq4, 1);
+        
+        assertEquals(((LeafNode) ((InternalNode) n).g()).dna(), seq3);
     }
 
     /**
@@ -128,7 +293,17 @@ public class InternalNodeTest {
      */
     @Test
     public void testC() {
-        fail("Not yet implemented");
+        InternalNode n = new InternalNode();
+        char[] seq1 = new char[]{'A', 'A', 'C', 'T'};
+        n.insert(seq1, 1);
+        char[] seq2 = new char[]{'C', 'A', 'C', 'T'};
+        n.insert(seq2, 1);
+        char[] seq3 = new char[]{'G', 'G', 'C'};
+        n.insert(seq3, 1);
+        char[] seq4 = new char[]{'T', 'G', 'C'};
+        n.insert(seq4, 1);
+        
+        assertEquals(((LeafNode) ((InternalNode) n).c()).dna(), seq2);
     }
 
     /**
@@ -136,7 +311,17 @@ public class InternalNodeTest {
      */
     @Test
     public void testT() {
-        fail("Not yet implemented");
+        InternalNode n = new InternalNode();
+        char[] seq1 = new char[]{'A', 'A', 'C', 'T'};
+        n.insert(seq1, 1);
+        char[] seq2 = new char[]{'C', 'A', 'C', 'T'};
+        n.insert(seq2, 1);
+        char[] seq3 = new char[]{'G', 'G', 'C'};
+        n.insert(seq3, 1);
+        char[] seq4 = new char[]{'T', 'G', 'C'};
+        n.insert(seq4, 1);
+        
+        assertEquals(((LeafNode) ((InternalNode) n).t()).dna(), seq4);
     }
 
     /**
@@ -144,7 +329,19 @@ public class InternalNodeTest {
      */
     @Test
     public void test$() {
-        fail("Not yet implemented");
+        InternalNode n = new InternalNode();
+        char[] seq1 = new char[]{'A', 'A', 'C'};
+        n.insert(seq1, 1);
+        char[] seq2 = new char[]{'C', 'A', 'T'};
+        n.insert(seq2, 1);
+        char[] seq3 = new char[]{'G', 'G', 'C'};
+        n.insert(seq3, 1);
+        char[] seq4 = new char[]{'T', 'G', 'C'};
+        n.insert(seq4, 1);
+        char[] seq5 = new char[]{'A'};
+        n.insert(seq5, 1);
+        
+        assertEquals(((LeafNode) ((InternalNode) n.a()).$()).dna(), seq5);
     }
 
     /**
@@ -152,7 +349,11 @@ public class InternalNodeTest {
      */
     @Test
     public void testIsLeaf() {
-        fail("Not yet implemented");
+        InternalNode n = new InternalNode();
+        char[] seq1 = new char[]{'A', 'G', 'C', 'T'};
+        n.insert(seq1, 1);
+        
+        assertTrue(n.a().isLeaf());
     }
 
     /**
@@ -160,7 +361,16 @@ public class InternalNodeTest {
      */
     @Test
     public void testIsFlyweight() {
-        fail("Not yet implemented");
+        InternalNode n = new InternalNode();
+        char[] seq1 = new char[]{'A', 'G', 'C', 'T'};
+        n.insert(seq1, 1);
+        
+        assertTrue(n.c().isFlyweight());
+        assertTrue(n.g().isFlyweight());
+        assertTrue(n.t().isFlyweight());
+        assertTrue(n.$().isFlyweight());
     }
+
+    
 
 }
