570546722f37151a258a8dba4cc891d86188eaf1
Julia Mahon Kuzin <juliam8@vt.edu>
2019-03-18 16:51:05 -0400

    made two new tests

---
+++ CS3114-Project2/src/InternalNodeTest.java
@@ -0,0 +1,161 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author jmkuz
+ *
+ */
+public class InternalNodeTest {
+
+    /**
+     * Test method for {@link InternalNode#InternalNode()}.
+     */
+    @Test
+    public void testInternalNode() {
+        DNATreeNode n = new InternalNode();
+        assertFalse(n.isFlyweight());
+        assertFalse(n.isLeaf());
+    }
+
+    /**
+     * Test method for {@link InternalNode#InternalNode(DNATreeNode, DNATreeNode, DNATreeNode, DNATreeNode, DNATreeNode)}.
+     */
+    @Test
+    public void testInternalNodeDNATreeNodeDNATreeNodeDNATreeNodeDNATreeNodeDNATreeNode() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link InternalNode#insert(char[], int)}.
+     */
+    @Test
+    public void testInsert() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link InternalNode#remove(char[], int)}.
+     */
+    @Test
+    public void testRemove() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link InternalNode#search(char[], int, SequenceSearch)}.
+     */
+    @Test
+    public void testSearch() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link InternalNode#print(boolean, boolean)}.
+     */
+    @Test
+    public void testPrint() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link InternalNode#setA(DNATreeNode)}.
+     */
+    @Test
+    public void testSetA() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link InternalNode#setG(DNATreeNode)}.
+     */
+    @Test
+    public void testSetG() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link InternalNode#setC(DNATreeNode)}.
+     */
+    @Test
+    public void testSetC() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link InternalNode#setT(DNATreeNode)}.
+     */
+    @Test
+    public void testSetT() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link InternalNode#set$(DNATreeNode)}.
+     */
+    @Test
+    public void testSet$() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link InternalNode#a()}.
+     */
+    @Test
+    public void testA() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link InternalNode#g()}.
+     */
+    @Test
+    public void testG() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link InternalNode#c()}.
+     */
+    @Test
+    public void testC() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link InternalNode#t()}.
+     */
+    @Test
+    public void testT() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link InternalNode#$()}.
+     */
+    @Test
+    public void test$() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link InternalNode#isLeaf()}.
+     */
+    @Test
+    public void testIsLeaf() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link InternalNode#isFlyweight()}.
+     */
+    @Test
+    public void testIsFlyweight() {
+        fail("Not yet implemented");
+    }
+
+}
---
+++ CS3114-Project2/src/LeafNodeTest.java
@@ -0,0 +1,127 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author jmkuz
+ *
+ */
+public class LeafNodeTest {
+
+    /**
+     * Test method for {@link LeafNode#LeafNode(char[])}.
+     */
+    @Test
+    public void testLeafNode() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link LeafNode#insert(char[], int)}.
+     */
+    @Test
+    public void testInsert() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link LeafNode#remove(char[], int)}.
+     */
+    @Test
+    public void testRemove() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link LeafNode#print(boolean, boolean)}.
+     */
+    @Test
+    public void testPrint() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link LeafNode#search(char[], int, SequenceSearch)}.
+     */
+    @Test
+    public void testSearchCharArrayIntSequenceSearch() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link LeafNode#search(char[], int)}.
+     */
+    @Test
+    public void testSearchCharArrayInt() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link LeafNode#setDNA(char[])}.
+     */
+    @Test
+    public void testSetDNA() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link LeafNode#dna()}.
+     */
+    @Test
+    public void testDna() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link LeafNode#isLeaf()}.
+     */
+    @Test
+    public void testIsLeaf() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link LeafNode#isFlyweight()}.
+     */
+    @Test
+    public void testIsFlyweight() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link LeafNode#percentA()}.
+     */
+    @Test
+    public void testPercentA() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link LeafNode#percentC()}.
+     */
+    @Test
+    public void testPercentC() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link LeafNode#percentG()}.
+     */
+    @Test
+    public void testPercentG() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link LeafNode#percentT()}.
+     */
+    @Test
+    public void testPercentT() {
+        fail("Not yet implemented");
+    }
+
+}
