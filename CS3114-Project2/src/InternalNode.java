9056c28551d4a03849b2b5adacfafa5dbd35b2ee
abbym1 <abbym1@vt.edu>
2019-03-18 22:45:12 -0400

    remove

--- CS3114-Project2/src/InternalNode.java
+++ CS3114-Project2/src/InternalNode.java
@@ -82,10 +82,10 @@ public class InternalNode implements DNATreeNode {
         else if (sequence[level-1] == 'T') {
             setT(t.remove(sequence, level + 1));
         }
-        if(level != 1)
+        //if(level != 1)
             return shrinkCheck();
-        else
-            return this;
+        //else
+         //   return this;
     }
     
     private DNATreeNode shrinkCheck() {
